15cb41640e754031cfeb8fdef26929ca70210b6d
tasniatapti <tasniatapti711@gmail.com>
2021-06-13 21:37:28 +0600

    lab task 3

---
+++ LAB_TASK_03_PHP_INTRO/task7.php
@@ -0,0 +1,31 @@
+<?php	
+for ($i=1; $i<=3; $i++)	
+{	 
+for($j=1;$j<=$i;$j++)	  
+{	  	
+echo " * ";	 
+}	  	
+echo "<br/>";   	
+}  
+for($i=1;$i<=3;$i++)
+    {
+        for($j=3;$j>=$i;$j--)
+        {
+            echo 4-$j." ";
+        }
+        echo"<br/> ";
+    }
+
+    echo"<br/> ";
+
+    $ch="A";
+    for($i=1;$i<=3;$i++)
+    {
+        for($j=1;$j<=$i;$j++)
+        {
+            echo$ch." ";
+            $ch++;
+        }
+        echo"<br/> ";
+    } 
+?>
