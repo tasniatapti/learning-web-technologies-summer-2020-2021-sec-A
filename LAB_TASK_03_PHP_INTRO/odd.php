7d1f63c29b8405b5f571b75b4748a2de42924bf2
tasniatapti <tasniatapti711@gmail.com>
2021-06-13 10:50:28 +0600

    Lab tas 3

---
+++ LAB_TASK_03_PHP_INTRO/odd.php
@@ -0,0 +1,10 @@
+<?php
+for($i=10; $i<=100; $i++){
+    if($i%2 == 1){// 
+        echo $i,"<br/>";
+    }
+	else
+	{
+		echo "This is even";
+ }
+?>
---
+++ LAB_TASK_03_PHP_INTRO/rectangle.php
@@ -0,0 +1,12 @@
+<?php
+
+$length=14;
+$width=13;
+echo "area of rectangle is $length*$width=". 2*($length+$width)."<br>";
+
+
+
+
+
+
+?>
