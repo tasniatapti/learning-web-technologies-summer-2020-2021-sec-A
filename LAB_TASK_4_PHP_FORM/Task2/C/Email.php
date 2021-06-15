e808fabe408b99ee705f50868b82ee6f73dea63b
tasniatapti <tasniatapti711@gmail.com>
2021-06-15 19:27:17 +0600

    lab task 4

---
+++ LAB_TASK_4_PHP_FORM/Task2/C/Email.php
@@ -0,0 +1,28 @@
+<?php
+	
+	$email = "";
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$email = $_REQUEST['email'];
+		if($email == ""){
+			echo "invalid email!";
+		}
+	}else{
+		echo 'invalid request';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email Form</title>
+</head>
+<body>
+	<form method="post" action="#" >
+
+		Email: <input type="text" email="email" value="" />
+				<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
